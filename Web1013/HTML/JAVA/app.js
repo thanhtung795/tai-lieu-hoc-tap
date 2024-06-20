082ef99066f1dcc4a22a4e48d15adde51c12f6fc
thanhtung795 <vothanhtung795@gmail.com>
2024-06-20 08:50:24 +0700

    first commit

---
+++ Web1013/HTML/JAVA/app.js
@@ -0,0 +1,11 @@
+//Bài 4
+//set a vallue of 10
+// let a = 10;
+// console.log(a);
+// alert(a);
+
+//bài 5
+//Nhập dữ liệu
+var x = +prompt("Nhập giá trị: ");
+//Xuất dữ liệu ra màn hình
+document.write(x);
